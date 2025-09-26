d53de8b797ac6209b9a8c66bdcb27e28ff87f295
andres felipe velez <andrew.vm1611@gmail.com>
2025-09-25 19:25:46 -0500

    primera parte

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -7,12 +7,48 @@ from app.services.util import generate_unique_id, date_lower_than_today_error, e
 
 
 # TODO: Implement Reminder class here
-
+@dataclass()
+class Reminder:
+    EMAIL: ClassVar[str] = "email"
+    SYSTEM: ClassVar[str] = "system"
+    date_time : datetime
+    type: str = EMAIL
+ #se cumple que regresa una cadena de texto
+    def __str__(self) -> str:
+        return f"Reminder on {self.date_time} of type {self.type}"
 
 # TODO: Implement Event class here
-
+@dataclass()
+class Event:
+    title:str
+    description:str
+    date_:date
+    start_at:time
+    end_at:time
+    reminders:list[Reminder]
+   id: str= generate_unique_id()
+   def add_reminder (self, reminder: Reminder) -> None:
+       reminder=Reminder(date_time,type)
+       self.reminders.append(reminder)
+   def delete_reminder (self, reminder_index: str) -> None:
 
 # TODO: Implement Day class here
+class Day:
+#el date_ inicia con un parametro en el constructor obligatoriamente
+    def __init__(self, date_:date):
+        self.date_ = date_
+        pass
 
 
 # TODO: Implement Calendar class here
+class Calendar:
+    days: dict[date, Day]
+    events: dict[str, Event]
+    def add_event(self, title:str, description:str,date_:date,start_at:time,end_at:time):
+      if date_<date.now():
+          date_lower_than_today_error()
+      if date_ not in self.days:
+          self.days[date_] = Day(date_)
+          new_event = Event(title, description, date_, start_at, end_at)
+          self.days[date_].add_event(new_event)
+
